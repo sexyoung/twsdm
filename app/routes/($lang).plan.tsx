3745aa50ea9b6d5a62b22b7a455747a013475072
sexyoung <Habuche@gmail.com>
2023-07-28 00:57:05 +0800

    feat: 計畫自由選先寫一個 table

--- app/routes/($lang).plan.tsx
+++ app/routes/($lang).plan.tsx
@@ -42,10 +42,128 @@ export default function () {
           </Link>
           <div />
         </div>
-        <div className="plan-table">
-          <img id="p1" src={p1} alt="經痛" />
-          <img id="p2" src={p2} alt="異常出血" />
-          <img id="p3" src={p3} alt="巧克力囊腫" />
+        <div
+          className="my-20 flex items-start whitespace-nowrap rounded-lg border border-red-200"
+          id="table1"
+        >
+          <table cellPadding={4} className="text-right">
+            <thead>
+              <tr>
+                <th className="bg-red-50">　</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="px-4">代表藥物</td>
+              </tr>
+              <tr>
+                <td className="px-4">奏效速度</td>
+              </tr>
+              <tr>
+                <td className="px-4">效力</td>
+              </tr>
+              <tr>
+                <td className="px-4">副作用</td>
+              </tr>
+              <tr>
+                <td className="px-4">使用方式</td>
+              </tr>
+              <tr>
+                <td className="px-4">價錢</td>
+              </tr>
+              <tr>
+                <td className="px-4">推薦順序</td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="overflow-x-auto overflow-y-hidden">
+            <table cellPadding={4} className="w-full text-center">
+              <thead>
+                <tr className="bg-red-50">
+                  <th>症狀控制藥</th>
+                  <th>第四代黃體素</th>
+                  <th>避孕藥</th>
+                  <th>子宮內投藥避孕器</th>
+                  <th>性腺激素釋放素促進劑</th>
+                  <th>(類)雄性素</th>
+                  <th>保守性手術</th>
+                  <th>根除性手術</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Naproxen (能百鎮)</td>
+                  <td>Dienogest (異位寧)</td>
+                  <td>Diane (黛麗安)</td>
+                  <td>Mirena (蜜蕊娜)</td>
+                  <td>Leuprolide (柳培林)</td>
+                  <td>Gestrinone (佑汝)</td>
+                  <td>肌腺瘤切除、神經阻斷等</td>
+                  <td>子宮切除</td>
+                </tr>
+                <tr>
+                  <td>快</td>
+                  <td>快</td>
+                  <td>數天</td>
+                  <td>數天</td>
+                  <td>3週</td>
+                  <td>2週</td>
+                  <td>快</td>
+                  <td>快</td>
+                </tr>
+                <tr>
+                  <td>有效</td>
+                  <td>極有效</td>
+                  <td>極有效</td>
+                  <td>極有效</td>
+                  <td>極有效</td>
+                  <td>極有效</td>
+                  <td>有效</td>
+                  <td>極有效</td>
+                </tr>
+                <tr>
+                  <td>輕微</td>
+                  <td>一般</td>
+                  <td>一般</td>
+                  <td>一般</td>
+                  <td>一般</td>
+                  <td>稍強</td>
+                  <td>稍強</td>
+                  <td>稍強</td>
+                </tr>
+                <tr>
+                  <td>經期每天口服</td>
+                  <td>每天口服</td>
+                  <td>每天口服</td>
+                  <td>放置一支持續5年</td>
+                  <td>每月一針共3-6針</td>
+                  <td>每天口服</td>
+                  <td>微創或傳統手術</td>
+                  <td>微創或傳統手術</td>
+                </tr>
+                <tr>
+                  <td>健保</td>
+                  <td>健保</td>
+                  <td>健保</td>
+                  <td>每支6000元以上</td>
+                  <td>每針6000元以上</td>
+                  <td>健保</td>
+                  <td>通常萬元起跳</td>
+                  <td>通常萬元起跳</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>2</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>3</td>
+                  <td>3</td>
+                  <td>3</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
