8a9eb7d22d9de13a33427931f3dc59b00d295c30
sexyoung <Habuche@gmail.com>
2023-08-10 22:08:54 +0800

    feat: 加上 子宮內膜異位症

--- app/routes/($lang)._index.tsx
+++ app/routes/($lang)._index.tsx
@@ -8,6 +8,20 @@ import icon2 from "~/images/home-2.png";
 import icon3 from "~/images/home-3.png";
 import icon4 from "~/images/home-4.png";
 
+import endometriosis0 from "~/images/endometriosis-0.png";
+import endometriosis1 from "~/images/endometriosis-1.png";
+import endometriosis2 from "~/images/endometriosis-2.png";
+import endometriosis3 from "~/images/endometriosis-3.png";
+import endometriosis4 from "~/images/endometriosis-4.png";
+
+const endometriosis = [
+  endometriosis0,
+  endometriosis1,
+  endometriosis2,
+  endometriosis3,
+  endometriosis4,
+];
+
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
@@ -21,8 +35,32 @@ export default function () {
         <div className="text">{t("title2")}</div>
         <div className="text-lg md:text-lg lg:text-2xl">{t("title3")}</div>
       </div>
-      <div className="my-10 text-center text-4xl font-bold text-red-500">
-        @todo 子宮內膜異位症
+      <div className="section container mx-auto  max-w-4xl">
+        <div className="my-2 text-center text-2xl font-medium md:text-4xl">
+          子宮內膜異位症？
+        </div>
+        <div className="mt-10 flex flex-wrap gap-x-4">
+          {[...Array(5).keys()].map((index) => (
+            <div
+              className="mb-10 flex-shrink-0 flex-grow-[1] basis-[calc(33.333333%-20px)]"
+              key={index}
+            >
+              <div className="img text-center">
+                <img
+                  src={endometriosis[index]}
+                  alt={endometriosis[index]}
+                  className="mx-auto inline-block max-w-[200px]"
+                />
+              </div>
+              <div className="text-center text-2xl font-medium">
+                {t(`endometriosis.${index}.title`)}
+              </div>
+              <ul className="desc">
+                <li>{t(`endometriosis.${index}.text`)}</li>
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="section container mx-auto  max-w-4xl">
         <h2 className="first-title">
@@ -31,27 +69,15 @@ export default function () {
           <div className="lg:inline">{t("3step")}</div>
         </h2>
         <div className="first-section mt-10 block md:flex md:gap-7">
-          <div className="mb-10 flex-1">
-            <div className="img" />
-            <div className="title">{t("3stepArticle.0.title")}</div>
-            <ul className="desc">
-              <li>{t("3stepArticle.0.text")}</li>
-            </ul>
-          </div>
-          <div className="mb-10 flex-1">
-            <div className="img" />
-            <div className="title">{t("3stepArticle.1.title")}</div>
-            <ul className="desc">
-              <li>{t("3stepArticle.1.text")}</li>
-            </ul>
-          </div>
-          <div className="mb-10 flex-1">
-            <div className="img" />
-            <div className="title">{t("3stepArticle.2.title")}</div>
-            <ul className="desc">
-              <li>{t("3stepArticle.2.text")}</li>
-            </ul>
-          </div>
+          {[...Array(3).keys()].map((index) => (
+            <div className="mb-10 flex-1" key={index}>
+              <div className="img" />
+              <div className="title">{t(`3stepArticle.${index}.title`)}</div>
+              <ul className="desc">
+                <li>{t(`3stepArticle.${index}.text`)}</li>
+              </ul>
+            </div>
+          ))}
         </div>
         <div className="text-center">
           <Link className="link-btn" to="/plan">
